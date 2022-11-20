7360c79ebb317cd64cf27d561ea11a8fc6be446c
D3FaltXD <kalistoroy619@gmail.com>
2022-11-20 22:05:32 +0530

    Updated Project 5

--- Projects/Project-5.py
+++ Projects/Project-5.py
@@ -1,35 +1,42 @@
 #This is a guess the number game.
 import random
 
-guessesTaken = 0
 
-print('Hello!')
-upper=int(input("Enter upper range: "))
-lower=int(input("Enter lower range: "))
+print("Hello!")
+
 
 myName=input("Enter Your name: ")
-number = random.randint(upper,lower)
-print('Well, ' + myName + ' I am thinking of a number between 1 and 20.')
+print("\nWelcome, " + myName)
+choice="yes"
+score=0
+while(True):
+    if(choice=="no"):
+        break
+    upper=int(input("Enter lower range: "))
+    lower=int(input("Enter upper range: "))
+    guessesTaken = 0
+    number = random.randint(upper,lower)
+    while guessesTaken < 3:
+        print("\nTake a guess.") #There are four spaces in front of print.
+        guess = input()
+        guess = int(guess)
 
-while guessesTaken < 6:
-    print('Take a guess.') #There are four spaces in front of print.
-    guess = input()
-    guess = int(guess)
+        guessesTaken = guessesTaken + 1
 
-    guessesTaken = guessesTaken + 1
-
-    if guess < number:
-        print('Your guess is too low.') # There are eight space in front of print.
-    if guess > number:
-        print('Your guess is too high.')
+        if guess < number:
+            print("\nHave one more try, Your guess is too low.") # There are eight space in front of print.
+        elif guess > number:
+            print("\nHave one more try, Your guess is too high.")
+        else:
+            break
 
     if guess == number:
-        break
+        number = str(number)
+        print("\nCongrat's")
+        score+=5
 
-if guess == number:
-    guessesTaken = str(guessesTaken)
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
-
-if guess != number:
-    number = str(number)
-    print('Nope. The number I was thinking of was ' + number)                
+    else: 
+        number = str(number)
+        print("\nBetter Luck next time")        
+    print("Score is %d"%score)
+    choice=input("Do you want to play again? yes-no :")
