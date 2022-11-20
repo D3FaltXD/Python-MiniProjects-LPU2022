3b934e30bcc947590235fc2ffc15c055f3808104
D3FaltXD <kalistoroy619@gmail.com>
2022-11-20 23:05:27 +0530

    Update Project-21.py

--- Projects/Project-21.py
+++ Projects/Project-21.py
@@ -10,14 +10,14 @@ else:
 if(N>cakeangle): #Only when N is greater tha cake angle cake can't be cut into pieces
     print("NO the cake will not cut in any piece of size %d"%N)
 else:
-    print("YES the cake will not cut in any piece of size %d"%N)
+    print("YES the cake will cut in any piece of size %d"%N)
 n=1 # start subratcrting the cake
 for i in range(N):
     cakeangle-=n
     n+=1
     if(cakeangle<0):
-        print("NO the cake would not cut into %d pieces such that no two of them are equal"%N)
+        print("NO the cake will not cut into %d pieces such that no two of them are equal"%N)
         break
 else:
-        print("YES the cake would not cut into %d pieces such that no two of them are equal"%N)
+        print("YES the cake will cut into %d pieces such that no two of them are equal"%N)
     
