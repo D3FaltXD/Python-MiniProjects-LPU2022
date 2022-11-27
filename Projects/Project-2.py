79c5624197357405982459a51ec057248d8c28cf
manoj kumar reddy <115407940+kora-manoj-kumar-reddy@users.noreply.github.com>
2022-11-27 15:03:50 +0530

    Update Project-2.py
    TO decrease length of code

--- Projects/Project-2.py
+++ Projects/Project-2.py
@@ -1,60 +1,51 @@
-# Rock Paper Scissor
-
-import time
-from random import *
-from os import system
-print("\n \n Welcome To The Game Of Rock Paper And Scissor \n First to Score 3 Points wins!")
-c=h=0
-options=["Scissors","Paper","Rock"]
-while(c!=3 and h!=3):
-    print("\n \n Press: \n 0 for Scissor \n 1 for Rock \n 2 for Rock \n")
-    g=int(input())
-    gc=randint(0,2)
-    if(g>2):
-        system('cls')
-        print("\n \n Invalid Input try again!")
-        print("\n \nScore Board: \n ---------------- \n| Human Score:",h,"|"," \n ---------------- \n| Comp  Score:",c,"|\n ---------------- \n")
-        continue
-    system('cls')
-    match options[g]:
-        case "Scissors":
-            if(gc==0):
-                print("\n \n Draw try again!")
-            elif(gc==1):
-                print("\n \n Human : Scissor Cuts Through Paper!")
-                print("\n User +1 Point")
-                h+=1
-            else:
-                print("\n \n CPU : Rock Smashes Scissor!")
-                print("\n Comp +1 Point")
-                c+=1
-        case "Paper":
-            if(gc==0):
-                print("\n \n CPU : Scissor Cuts Through Paper!")
-                print("\n Comp +1 Point")
-                c+=1
-            elif(gc==1):
-                print("\n \n Draw try again!")
-                
-            else:
-                print("\n \n Human : Paper Covers Rock!")
-                print("\n User +1 Point")
-                h+=1
-        case "Rock":
-            if(gc==0):
-                print("\n \n Human : Rock Smashes Scissor!")
-                print("\n User +1 Point")
-                h+=1
-            elif(gc==1):
-                print("\n \n CPU : Scissor Cuts Through Paper!")
-                print("\n Comp +1 Point")
-                c+=1
-            else:
-                print("\n \n Draw try again!")
-    print("\n \nScore Board: \n ---------------- \n| Human Score:",h,"|"," \n ---------------- \n| Comp  Score:",c,"|\n ---------------- \n")
-    time.sleep(1)
-if(h==3):
-    print("Human Wins! \n \n")
+import random
+count1 = 0
+count2 = 0
+count3 = 0
+round = 1
+print("Rock, Paper or Scissors?")
+n = int(input("How many rounds you want to play? "))
+while(round <= n):
+    print("Round:",round)
+    player1 = input("User: ").title()
+    a=["Rock","Paper","Scissors"]
+    while player1 not in a:
+            player1 = input("Insert valid: ").title()
+    player2 = random.choice(["Rock", "Paper", "Scissors"]).title()
+    print("CPU: ", player2)
+    round += 1
+    if player1 == player2:
+        count3 += 1
+        print("It's a Tie")     
+    elif player1 == "Rock":
+        if player2 == "Scissors":
+            count2 += 1
+            print("Rock smashes scissors! You win!")
+        else:
+            count1 += 1
+            print("Paper covers rock! You lose.")
+    elif player1 == "Paper":
+        if player2 == "Rock":
+            count2 += 1
+            print("Paper covers rock! You win!")
+        else:
+            count1 += 1
+            print("Scissors cuts paper! You lose.")
+    elif player1 == "Scissors":
+        if player2 == "Paper":
+            count2 += 1
+            print("Scissors cuts paper! You win!")
+        else:
+            count1 += 1
+            print("Rock smashes scissors! You lose.")
+print("End the game")
+print("*****FINAL SCORES*****")
+print("User Score:",count2)
+print("CPU Score:",count1)
+print("Ties:",count3)
+if count2>count1:
+    print("*********************You won*******************")
+elif count2<count1:
+    print("*******************You lost,AI won*******************")
 else:
-    print("Computer Wins! \n \n ")                
-            
+    print("*******************It's a tie*******************")
