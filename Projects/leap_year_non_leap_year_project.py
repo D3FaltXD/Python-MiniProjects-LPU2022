68cd1fa29172a74667203888ec227adc77f612f5
Nilesh kumar gupta <113936379+Nilesh220@users.noreply.github.com>
2022-12-18 08:57:35 +0530

    Add files via upload

---
+++ Projects/leap_year_non_leap_year_project.py
@@ -0,0 +1,24 @@
+date1 = input("Enter your starting year: ")                     # taking input from user for starting year
+date2 = input("Enter your ending year: ")                       # taking input from user for ending year
+d1 = date1.split("/")
+d2 = date2.split("/")
+y1 = int(d1[2])
+y2 = int(d2[2])
+leap = []                                                                 # creating empty list
+non_leap = []                                                             # creating empty list
+for i in range(y1, y2+1):                                             # loop for starting year to ending year
+    if i % 4 == 0:                                                  # conditions for checking leap year or not
+        if i % 100 != 0:
+            leap.append(i)
+        else:
+            if i % 400 == 0:
+                leap.append(i)                                      # using append function to store value in the list
+            else:
+                non_leap.append(i)
+    else:
+        non_leap.append(i)
+print("Leap years are:",leap)                                         # printing leap year
+print("Non-Leap years are:",non_leap)                                 # printing non- leap years
+
+
+        
